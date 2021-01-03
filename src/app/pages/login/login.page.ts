08547e6a989238262823272e6c79e7a576179a41
Miguel Gomez <mgomez.developer@gmail.com>
2021-01-03 16:50:21 -0500

    Add form Validation and improve UI for Login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  AlertController,
+  LoadingController,
+  ToastController,
+} from "@ionic/angular";
 
 @Component({
   selector: "app-login",
@@ -9,6 +14,67 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 export class LoginPage implements OnInit {
   public loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
-  ngOnInit() {}
+  constructor(
+    private alertController: AlertController,
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.validateFormFields();
+  }
+
+  validateFormFields() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
+
+  async forgotCredentialsModal() {
+    const alert = await this.alertController.create({
+      subHeader: "Forgot Password?",
+      message:
+        "Enter email address you used to register, and we will send a link to reset your password",
+      inputs: [
+        {
+          name: "email",
+          type: "email",
+          placeholder: "customer@domain.com",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Customer canceled request for forgotten password");
+          },
+        },
+        {
+          text: "Confirm",
+          handler: async () => {
+            const loader = await this.loadingController.create({
+              duration: 2000,
+            });
+
+            loader.present();
+            loader.onWillDismiss().then(async () => {
+              const toast = await this.toastController.create({
+                message: "Check your email for recovery instructions",
+                duration: 3000,
+                position: "bottom",
+              });
+
+              toast.present();
+            });
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
