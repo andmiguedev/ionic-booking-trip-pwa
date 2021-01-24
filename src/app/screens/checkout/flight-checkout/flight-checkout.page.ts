083e365a27ab91ed14a441dd5dc66302dd5a5706
Miguel Gomez <mgomez.developer@gmail.com>
2021-01-24 11:40:38 -0500

    Create and design initial Flight checkout mobile page

--- src/app/screens/checkout/flight-checkout/flight-checkout.page.ts
+++ src/app/screens/checkout/flight-checkout/flight-checkout.page.ts
@@ -1,8 +1,30 @@
-import { Component } from "@angular/core";
+import { ReturnService } from "./../../../services/data/returns/return.service";
+import { DepartureService } from "./../../../services/data/departures/departure.service";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-flight-checkout",
   templateUrl: "./flight-checkout.page.html",
   styleUrls: ["./flight-checkout.page.scss"],
 })
-export class FlightCheckoutPage {}
+export class FlightCheckoutPage {
+  public departingFlight: any;
+  public returningFlight: any;
+  passengers: number = 1;
+
+  constructor(
+    private departureService: DepartureService,
+    private returnService: ReturnService
+  ) {
+    this.departingFlight = this.departureService.getRecentDepartures()[0];
+    this.returningFlight = this.returnService.getRecentReturns()[0];
+  }
+
+  addPassenger() {
+    this.passengers++;
+  }
+
+  removePassenger() {
+    this.passengers--;
+  }
+}
